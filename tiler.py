ca9c5c75c0ff0111d5112787f54e1c854f1ff508
Leif Denby <leif@denby.eu>
2019-02-28 12:37:13 +0100

    cropping before resample

--- tiler.py
+++ tiler.py
@@ -103,10 +103,38 @@ class Tile():
 
         return ds
 
-    def resample(self, da, N, method='bilinear'):
+    def _crop_input(self, da, pad_pct=0.1):
+        xs, ys, _ = da.crs.transform_points(ccrs.PlateCarree(),
+                                            *self.get_bounds().T
+                                           ).T
+        x_min, x_max = np.min(xs), np.max(xs)
+        y_min, y_max = np.min(ys), np.max(ys)
+        lx = x_max - x_min
+        ly = y_max - y_min
+
+        x_min -= pad_pct*lx
+        y_min -= pad_pct*ly
+        x_max += pad_pct*lx
+        y_max += pad_pct*ly
+
+        if da.x[0] > da.x[-1]:
+            x_slice = slice(x_max, x_min)
+        else:
+            x_slice = slice(x_min, x_max)
+
+        if da.y[0] > da.y[-1]:
+            y_slice = slice(y_max, y_min)
+        else:
+            y_slice = slice(y_min, y_max)
+
+        return da.sel(x=x_slice, y=y_slice)
+
+    def resample(self, da, N, method='bilinear', crop_pad_pct=0.1):
         """
         Resample a xarray DataArray onto this tile with grid made of NxN points
         """
+        da = self._crop_input(da=da, pad_pct=crop_pad_pct)
+
         old_grid = xr.Dataset(coords=da.coords)
 
         if not hasattr(da, 'crs'):
