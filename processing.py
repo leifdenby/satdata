87a9de28dbfd2f9afdf56e323a08540e6fc69081
Leif Denby <leif@denby.eu>
2019-03-08 14:44:43 +0000

    refactor our multiprocessing util

--- processing.py
+++ processing.py
@@ -10,6 +10,7 @@ import time
 import yaml
 
 from . import tiler
+from .multiprocessing import parallel
 
 
 def set_projection_attribute_and_scale_coords(ds):
@@ -142,34 +143,3 @@ def triplet_fn(kws, *args, **kwargs):
     meta_fn = tile_path/"{:05d}_meta.yaml".format(triplet_n)
     with open(meta_fn, 'w') as fh:
         yaml.dump(meta, fh, default_flow_style=False)
-
-
-
-from fastprogress.fastprogress import master_bar, progress_bar
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
-from typing import Collection, Any
-import os
-from types import SimpleNamespace
-import concurrent.futures
-
-def ifnone(a:Any,b:Any)->Any:
-    "`a` if `a` is not None, otherwise `b`."
-    return b if a is None else a
-
-def num_cpus()->int:
-    "Get number of cpus"
-    try:                   return len(os.sched_getaffinity(0))
-    except AttributeError: return os.cpu_count()
-
-_default_cpus = min(16, num_cpus())
-defaults = SimpleNamespace(cpus=_default_cpus, cmap='viridis')
-
-def parallel(func, arr:Collection, max_workers:int=None):
-    "Call `func` on every element of `arr` in parallel using `max_workers`."
-    max_workers = ifnone(max_workers, defaults.cpus)
-    if max_workers<2: _ = [func(o,i) for i,o in enumerate(arr)]
-    else:
-        with ProcessPoolExecutor(max_workers=max_workers) as ex:
-            futures = [ex.submit(func,o,i) for i,o in enumerate(arr)]
-    for f in progress_bar(concurrent.futures.as_completed(futures), total=len(arr)):
-        f.result()
