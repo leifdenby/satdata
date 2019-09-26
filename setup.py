1fcef30f7cbde438b94ad663fa2a12f97b274a46
Leif Denby <leif@denby.eu>
2019-09-26 12:03:11 +0200

    add missing deps

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='satdata',
     packages=find_packages(exclude=['contrib', 'tests', 'docs']),
-    version='0.1.0',
+    version='0.1.1',
     description='Satellite data access from Amazon S3 via python',
     author='Leif Denby',
     author_email='leifdenby@gmail.com',
     url='https://github.com/leifdenby/satdata',
     classifiers=[],
-    install_requires=["satpy", "xarray", "xesmf"]
+    install_requires=["satpy", "xarray", "xesmf", "boto3", "tqdm",
+                      "progressbar"]
 )
