c8456ff726869dc518436895185880c376b05492
Leif Denby <leif@denby.eu>
2020-01-13 13:50:14 +0000

    bump version to v0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='satdata',
     packages=find_packages(exclude=['contrib', 'tests', 'docs']),
-    version='0.1.1',
+    version='0.1.2',
     description='Satellite data access from Amazon S3 via python',
     author='Leif Denby',
     author_email='leifdenby@gmail.com',
