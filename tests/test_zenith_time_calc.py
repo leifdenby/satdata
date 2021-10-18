40d14cb3bf134d9b67cd0a33f48acd23624cc88c
Leif Denby <leif@denby.eu>
2021-10-18 16:19:32 +0100

    remove numpy requirement

--- tests/test_zenith_time_calc.py
+++ tests/test_zenith_time_calc.py
@@ -1,17 +1,14 @@
 from satdata import utils
 import datetime
-import numpy as np
 
 
 def test_zenith_calc():
     t0 = datetime.datetime(year=2020, month=2, day=2, hour=12, minute=0)
 
     # move all the way around Earth
-    for lon in np.arange(-360.0, 360.0, 10.0):
-        print(lon)
+    for lon in range(-360, 360, 10):
         dt_zenith = utils.calc_zenith_time_offset_at_loc(lon=lon)
         t_zenith = t0 + dt_zenith
-        print(lon, t_zenith)
 
         # check that we've finding the correct zenith times from just 15min
         # before and after
